abdedc9e34abf9107999f6eb33aef61bc731535d
wi11mc <wj7@hawaii.edu>
2024-09-20 21:03:49 -0700

    added the user input and prints the answer

--- week4secondary.py
+++ week4secondary.py
@@ -3,3 +3,10 @@ def factorial(n):
     for i in range(1, n + 1):
         result *= i
     return result
+
+#to get the user input i will add a input option
+num = int(input("what number do you want to factorial"))
+
+#print the answer
+result = factorial(num)
+print(f"the factorial of {num} is {result}")
